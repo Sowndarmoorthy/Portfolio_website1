8683054cb428abeaf1a2ca38b623af4135f4e8f0
Sowndar <sowundarmoorthy@gmail.com>
2024-10-24 09:13:35 +0530

    Complete the Css and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import Services from "./components/UI/Services";
+import Portfolio from "./components/UI/Portfolio";
+import Contact from "./components/UI/Contact";
+
 
 function App() {
-  return (
-    <h1>hello</h1>
-  );
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return(
+  <>
+    <Header />
+    <main>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </main>
+    <Footer />
+  </>
+);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}'
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#8873ef",
+        headingColor: "#081e21",
+        smallTextColor: "#193256",
+      },
+    },
   },
   plugins: [],
-}
+};
